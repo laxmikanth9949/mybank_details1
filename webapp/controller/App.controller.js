428eb86758e37586e728ca3658ef3feb3cb49f7e
laxmi kanth <laxmikanth9949@gmail.com>
2023-09-23 11:21:00 +0000

    my first commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("com.bankdetails.mybankdetails1.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
